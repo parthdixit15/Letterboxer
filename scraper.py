5c4658bc5992c9f10aac75f7b0f93f9b091eceff
parthdixit15 <135131391+parthdixit15@users.noreply.github.com>
2024-01-14 12:26:47 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -15,41 +15,51 @@ from time import sleep
 import json
 
 
+#reading the file 
+data = read_csv("watchlist1.csv") 
 
-data = read_csv("watchlist1.csv")
+#extracting the links column from the csv file and saving it in a list
+links = data['Letterboxd URI'].tolist() 
 
-links = data['Letterboxd URI'].tolist()
+#initialising an empty dictionary
+streamings_available={} 
 
-streamings_available={}
-
-session = HTMLSession()
+#starting a html session
+session = HTMLSession() 
 
 streamings_available={}
 
 for link in links:
+    #making a get request
+    response = session.get(link)  
 
-    response = session.get(link)
+    #rendering the page with the link and making it sleep for 1 second incase the page is not rendered completely
+    response.html.render(sleep = 1) 
 
-    response.html.render(sleep = 1)
+    #parsing the page using beautiful soup
+    soup= BeautifulSoup(response.html.html,'lxml') 
 
-    soup= BeautifulSoup(response.html.html,'lxml')
+    #gettin the "services"
+    services=soup.find_all('p',class_='service')  
 
-    services=soup.find_all('p',class_='service')
+    #getting the movie title
+    name=soup.find('h1',class_='headline-1') 
 
-    name=soup.find('h1',class_='headline-1')
-
-    streamings_available.setdefault(name.text,[])
+    #initializing the movie with an empty list
+    streamings_available.setdefault(name.text,[]) #initializing the movie with an empty list
 
     for service in services:
 
-        service=service.find('desc')
+        service=service.find('desc')#getting service name
 
         if(service):
 
             streamings_available[name.text].append(service.text)
 
+    #delaying the program so not to get rate limited
     sleep(randint(1,3))      
 
+#creating a json file to store the list
 with open(r'C:\Users\Asus\OneDrive\Desktop\reptile\letterboxer\streamings_available.json','w') as f:
 
     json.dump(streamings_available,f)      
